8f67adfef8005c3187d72d06885640da6269b63c
luckylluuks <153525439+luckylluuks@users.noreply.github.com>
2024-08-16 09:51:28 +0000

    44

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-// import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-
-// import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152.1/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152.1/examples/jsm/controls/OrbitControls.js';   
 
 
 //scene
