07365ea9d658e0dafed2b6c60a64be8e16449d71
luckylluuks <153525439+luckylluuks@users.noreply.github.com>
2024-08-19 07:26:10 +0000

    098

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
 
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.167.1/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.167.1/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
 
 //scene
